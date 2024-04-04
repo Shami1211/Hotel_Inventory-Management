3978ae608035892d107b859fe3e9cf7c203def09
Shami1211 <it21203558@my.sliit.lk>
2024-04-04 15:16:08 +0530

    n

---
+++ Budget-Planing/frontend/src/components/Admin-Discount/Discount/Discount.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Discount = (props) => {
+  const { _id, image, name, company, price, discount, total, start, end } = props.discount;
+
+  return (
+    <div>
+      <img src={image} alt={name} onError={() => console.log('Error loading image:', image)} />
+      <h2>{name}</h2>
+      
+      <h4>Company: {company}</h4>
+      <h4>Price: ${price}</h4>
+      <h4>Discount: {discount}%</h4>
+      <h4>Total: ${total}</h4>
+      <h4>Start Date: {start}</h4>
+      <h4>End Date: {end}</h4>
+    </div>
+  );
+};
+
+export default Discount;
